cc922aee68a4db7bc18fdfdfd798381d7699889e
Red Daly <reddaly@gmail.com>
2014-04-01 18:43:13 -0700

    Remove codepage support from xlsx.

--- bits/32_codepage.js
+++ bits/32_codepage.js
@@ -1,4 +1,6 @@
+// What is this?
 var current_codepage, current_cptable, cptable;
+/*
 if(typeof module !== "undefined" && typeof require !== 'undefined') {
 	if(typeof cptable === 'undefined') cptable = require('codepage');
 	current_codepage = 1252; current_cptable = cptable[1252];
@@ -7,4 +9,4 @@ function reset_cp() {
 	current_codepage = 1252; if(typeof cptable !== 'undefined') current_cptable = cptable[1252];
 }
 function _getchar(x) { return String.fromCharCode(x); }
-
+*/
