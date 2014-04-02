326b5497de7f3850a359d1c00fdf594c98ae364b
Red Daly <reddaly@gmail.com>
2014-04-01 18:55:38 -0700

    Don't entirely obliterate codepage.

--- xlsx.js
+++ xlsx.js
@@ -495,11 +495,12 @@ if(typeof module !== "undefined" && typeof require !== 'undefined') {
 	if(typeof cptable === 'undefined') cptable = require('codepage');
 	current_codepage = 1252; current_cptable = cptable[1252];
 }
+*/
 function reset_cp() {
 	current_codepage = 1252; if(typeof cptable !== 'undefined') current_cptable = cptable[1252];
 }
 function _getchar(x) { return String.fromCharCode(x); }
-*/function getdata(data) {
+function getdata(data) {
 	if(!data) return null;
 	if(data.data) return data.name.substr(-4) !== ".bin" ? data.data : data.data.split("").map(function(x) { return x.charCodeAt(0); });
 	if(data.asNodeBuffer && typeof Buffer !== 'undefined' && data.name.substr(-4)===".bin") return data.asNodeBuffer();
