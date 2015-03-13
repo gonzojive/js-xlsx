ce1448b23cfef0b387de0c0b2ef6c08725d9a645
Red Daly <reddaly@gmail.com>
2015-03-13 02:34:01 -0700

    Revert "Remove codepage support from xlsx."
    This reverts commit cc922aee68a4db7bc18fdfdfd798381d7699889e.

--- bits/32_codepage.js
+++ bits/32_codepage.js
@@ -1,6 +1,4 @@
-// What is this?
 var current_codepage, current_cptable, cptable;
-/*
 if(typeof module !== "undefined" && typeof require !== 'undefined') {
 	if(typeof cptable === 'undefined') cptable = require('codepage');
 	current_codepage = 1252; current_cptable = cptable[1252];
@@ -9,4 +7,4 @@ function reset_cp() {
 	current_codepage = 1252; if(typeof cptable !== 'undefined') current_cptable = cptable[1252];
 }
 function _getchar(x) { return String.fromCharCode(x); }
-*/
+
--- xlsx.js
+++ xlsx.js
@@ -488,9 +488,7 @@ make_ssf(SSF);
 var XLSX = {};
 (function(XLSX){
 XLSX.version = '0.5.15';
-// What is this?
 var current_codepage, current_cptable, cptable;
-/*
 if(typeof module !== "undefined" && typeof require !== 'undefined') {
 	if(typeof cptable === 'undefined') cptable = require('codepage');
 	current_codepage = 1252; current_cptable = cptable[1252];
@@ -499,7 +497,8 @@ function reset_cp() {
 	current_codepage = 1252; if(typeof cptable !== 'undefined') current_cptable = cptable[1252];
 }
 function _getchar(x) { return String.fromCharCode(x); }
-*/function getdata(data) {
+
+function getdata(data) {
 	if(!data) return null;
 	if(data.data) return data.name.substr(-4) !== ".bin" ? data.data : data.data.split("").map(function(x) { return x.charCodeAt(0); });
 	if(data.asNodeBuffer && typeof Buffer !== 'undefined' && data.name.substr(-4)===".bin") return data.asNodeBuffer();
